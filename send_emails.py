e57819e1ea671bc1f5d53617be4d08a419180a9d
Levi <thegood878@gmail.com>
2023-06-28 19:20:09 -0300

    send_email function

--- send_emails.py
+++ send_emails.py
@@ -16,32 +16,41 @@ FILE_PATH = pathlib.Path(__file__).parent / 'email.html'
 # Sender and Recipient data
 sender = os.getenv('FROM_EMAIL', '')
 recipient = sender
+subject = "Send email with python"
 
-# SMTP Settings
-smtp_server = 'smtp.gmail.com'
-smtp_port = 587
-smtp_username = os.getenv('FROM_EMAIL', '')
-smtp_password = os.getenv('EMAIL_PASSWORD', '')
 
-# Text mensage
-with open(FILE_PATH, 'r') as file:
-    file_text = file.read()
-    template = Template(file_text)
-    email_text = template.substitute(name='Levi')
+def send_email(sender: str, recipient: str, recipient_name: str,
+               html_file: pathlib.Path, subject: str):
 
-# MIMEMultipart
-mime_multipart = MIMEMultipart()
-mime_multipart['from'] = sender
-mime_multipart['to'] = recipient
-mime_multipart['subject'] = 'Send email with python'
+    # SMTP Settings
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+    smtp_username = os.getenv('FROM_EMAIL', '')
+    smtp_password = os.getenv('EMAIL_PASSWORD', '')
 
-email_body = MIMEText(email_text, 'html', 'utf-8')
-mime_multipart.attach(email_body)
+    # Text mensage
+    with open(html_file, 'r') as file:
+        file_text = file.read()
+        template = Template(file_text)
+        email_text = template.substitute(name=recipient_name)
 
-# sent email
-with smtplib.SMTP(smtp_server, smtp_port) as server:
-    server.ehlo()
-    server.starttls()
-    server.login(smtp_username, smtp_password)
-    server.send_message(mime_multipart)
-    print('Email sucessfully sent!!🎉🎉')
+    # MIMEMultipart
+    mime_multipart = MIMEMultipart()
+    mime_multipart['from'] = sender
+    mime_multipart['to'] = recipient
+    mime_multipart['subject'] = subject
+
+    email_body = MIMEText(email_text, 'html', 'utf-8')
+    mime_multipart.attach(email_body)
+
+    # sent email
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.ehlo()
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+        server.send_message(mime_multipart)
+        print('Email sucessfully sent!!🎉🎉')
+
+
+if __name__ == '__main__':
+    send_email(sender, recipient, "Levi", FILE_PATH, subject)
